3540e82fb8d3e4ed8ab1499897ff7708e5c60177
Bitcoin Bill <bitcoinbill000@protonmail.com>
2020-01-04 19:14:38 +0000

    paramterise a few things

--- simulate.py
+++ simulate.py
@@ -9,6 +9,9 @@ balance_limit = 1000000
 start_balance = 1000
 stop_distance = 0.005
 liquidity_limit = 5000000
+normal_risk = 0.1
+increased_risk = 0.25
+trades_per_week = 2
 
 def run_simulation():
     balance = start_balance
@@ -16,15 +19,15 @@ def run_simulation():
     last_trade_winner = False
     while balance < balance_limit:
         num = random.randrange(1, 100)
-        risk = 0.1
+        risk = normal_risk
         # if the last trade was a winner we'll risk more of our account balance to compound returns aggressively
         if last_trade_winner is True:
-            risk = 0.25
+            risk = increased_risk
         # check the $ risk would not exceed the realistic liquidity of the market
         dollar_risk = risk * balance
-        position_size = dollar_risk / 0.005
+        position_size = dollar_risk / stop_distance
         if position_size > liquidity_limit:
-            dollar_risk = liquidity_limit * 0.005
+            dollar_risk = liquidity_limit * stop_distance
         if num < (win_rate * 100):
             # winning trade
             balance = round(balance + (risk_reward * dollar_risk))
@@ -40,7 +43,7 @@ def build_stats(simulations):
     stats = []
     for sim in simulations:
         trades = len(sim)
-        months = trades / 8
+        months = trades / trades_per_week / 4
         stats.append({
             'trades': trades,
             'months': months
